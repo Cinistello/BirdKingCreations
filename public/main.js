d687263a9ac597a66bdd10ee405538df2e110eb2
Cinistello <cinistello@gmail.com>
2025-10-03 19:23:16 -0700

    idk

--- public/main.js
+++ public/main.js
@@ -1,12 +1,16 @@
-function openHamburger() {
-  const menu = document.getElementById("hamburgerMenu");
-  menu.style.display = "flex";
-  menu.style.width = "250px";
+function toggleHamburger() {
+	const menu = document.getElementById("hamburgerMenu");
+    const menuStyle = window.getComputedStyle(menu);
 
-}
-
-function closeHamburger() {
-  const menu = document.getElementById("hamburgerMenu");
-  menu.style.width = "0";
-  menu.style.display = "none";
+	if (menuStyle.display === "none") {
+		menu.style.display = "flex";
+		menu.style.width = "100%";
+		document.body.style.overflow = 'hidden';
+	}
+	else {
+		const menu = document.getElementById("hamburgerMenu");
+		menu.style.width = "0";
+		menu.style.display = "none";
+		document.body.style.overflow = 'visible';
+	}
 }
