d4f15d252f0aa54d5d147eb19e814281f89e3c16
Cinistello <cinistello@gmail.com>
2026-08-07 20:09:50 -0700

    Deploy

---
+++ main.js
@@ -0,0 +1,16 @@
+function toggleHamburger() {
+	const menu = document.getElementById("hamburgerMenu");
+    const menuStyle = window.getComputedStyle(menu);
+
+	if (menuStyle.display === "none") {
+		menu.style.display = "flex";
+		menu.style.width = "40%";
+		document.body.style.overflow = 'hidden';
+	}
+	else {
+		const menu = document.getElementById("hamburgerMenu");
+		menu.style.width = "0";
+		menu.style.display = "none";
+		document.body.style.overflow = 'visible';
+	}
+}
